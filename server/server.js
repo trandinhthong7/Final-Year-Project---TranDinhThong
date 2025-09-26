b65023b3d6168a96e23e837dcb519d5589f3a931
Trần Đình Thông <trandinhthong@Trans-MacBook-Air.local>
2025-09-27 02:36:00 +0700

    first time

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const port = process.env.port || 3000;
+const app = express();
+
+app.listen(port);
+app.get('/',(req,res)=>{
+    res.send("Hello World");
+});
+
+console.log(`Server started on http://localhost:${port}`);
